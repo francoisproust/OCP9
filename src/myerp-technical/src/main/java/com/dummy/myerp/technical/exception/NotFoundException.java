908fa1254cd02306b00396e8f33f43c147d985e2
francois <francois.proust.72@gmail.com>
2020-09-25 09:58:54 +0200

    suppression constructeur par defaut

--- src/myerp-technical/src/main/java/com/dummy/myerp/technical/exception/NotFoundException.java
+++ src/myerp-technical/src/main/java/com/dummy/myerp/technical/exception/NotFoundException.java
@@ -9,12 +9,6 @@ public class NotFoundException extends Exception {
     /** serialVersionUID */
     private static final long serialVersionUID = 1L;
 
-    /**
-     * Constructeur.
-     */
-    public NotFoundException() {
-        super();
-    }
 
     /**
      * Constructeur.
